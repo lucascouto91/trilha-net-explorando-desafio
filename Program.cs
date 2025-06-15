3fa184f221d76c0c0918ca891e0dabf6e9bcfb2d
Lucas Couto <lucascouto91@gmail.com>
2025-06-15 13:46:12 -0300

    refactor: Rename variables for clarity

--- Program.cs
+++ Program.cs
@@ -13,13 +13,13 @@ hospedes.Add(p1);
 hospedes.Add(p2);
 
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Suite suite1 = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
-reserva.CadastrarSuite(suite);
-reserva.CadastrarHospedes(hospedes);
+Reserva reserva1 = new Reserva(diasReservados: 5);
+reserva1.CadastrarSuite(suite1);
+reserva1.CadastrarHospedes(hospedes);
 
 // Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+Console.WriteLine($"Hóspedes: {reserva1.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Valor diária: {reserva1.CalcularValorDiaria()}");
